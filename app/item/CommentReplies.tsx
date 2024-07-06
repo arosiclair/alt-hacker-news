8ac1cd1f650c04d7e6c34c8c54b0c88dff96317a
Andrew Rosiclair <arosiclair@gmail.com>
2024-07-06 16:52:54 -0400

    fetch replies

--- app/item/CommentReplies.tsx
+++ app/item/CommentReplies.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import DebugJSON from '../DebugJSON';
+import { HNItem } from '../types/hacker-news';
+import { fetchItems } from '../lib/API';
 
 type CommentRepliesProps = {
   replyIDs: number[];
@@ -9,7 +11,15 @@ type CommentRepliesProps = {
 
 export default function CommentReplies({ replyIDs }: CommentRepliesProps) {
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const [items, setItems] = useState<HNItem[]>([]);
+
   const toggleCollapsed = () => setIsCollapsed(!isCollapsed);
+
+  useEffect(() => {
+    if (!isCollapsed && !items.length) {
+      fetchItems(replyIDs).then(setItems);
+    }
+  }, [isCollapsed, items.length, replyIDs]);
 
   if (isCollapsed) {
     return (
@@ -19,7 +29,7 @@ export default function CommentReplies({ replyIDs }: CommentRepliesProps) {
 
   return (
     <div onClick={toggleCollapsed}>
-      <DebugJSON>{replyIDs}</DebugJSON>
+      <DebugJSON>{items}</DebugJSON>
     </div>
   );
 }
