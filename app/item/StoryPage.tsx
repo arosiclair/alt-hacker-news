fc28ec14f768141068559cfb6f0e84398d215418
Andrew Rosiclair <arosiclair@gmail.com>
2024-07-06 16:26:26 -0400

    add score

--- app/item/StoryPage.tsx
+++ app/item/StoryPage.tsx
@@ -1,4 +1,3 @@
-import DebugJSON from '../DebugJSON';
 import { HNItem } from '../types/hacker-news';
 import Comment from './Comment';
 import Paragraphs from './Paragraphs';
@@ -13,20 +12,14 @@ export default function StoryPage({ item }: StoryPageProps) {
 
   return (
     <div>
-      <div>
-        <h1>Hello, StoryPage!</h1>
-        <h2>ID: {item.id}</h2>
-        <h3>JSON:</h3>
-        <DebugJSON>{item}</DebugJSON>
-      </div>
-
       <h1>
         <a href={item.url} target="_blank" rel="noopener noreferrer">
           {item.title}
         </a>
       </h1>
       <h2>
-        by {item.by} • {item.time} • {numComments} {commentText}
+        by {item.by} • {item.time} • {item.score} points • {numComments}{' '}
+        {commentText}
       </h2>
 
       <Paragraphs rawText={item.text ?? ''} />
