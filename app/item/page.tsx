b46f77e6fc10d5ba6c31958dc5ad269c57756bc5
Andrew Rosiclair <arosiclair@gmail.com>
2024-07-06 13:24:21 -0400

    fetch and parse item json

--- app/item/page.tsx
+++ app/item/page.tsx
@@ -1,11 +1,21 @@
 import { PageProps } from '../types/misc';
 
-export default function ItemPage({ searchParams }: PageProps) {
+export default async function ItemPage({ searchParams }: PageProps) {
   const itemID = searchParams?.['id'] ?? '';
+  if (!itemID) {
+    return <h1>Not Found</h1>;
+  }
+
+  const response = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${itemID}.json`,
+  );
+  const json = await response.json();
+
   return (
     <>
       <h1>Hello, ItemPage!</h1>
       <h2>ID: {itemID}</h2>
+      <h2>JSON: {JSON.stringify(json)}</h2>
     </>
   );
 }
