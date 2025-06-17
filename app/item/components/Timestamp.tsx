7d39212c131d472fecad5b6fee2d1e3284704849
Andrew Rosiclair <arosiclair@gmail.com>
2025-06-16 20:39:05 -0400

    hopefully fix timestamps once and for all

--- app/item/components/Timestamp.tsx
+++ app/item/components/Timestamp.tsx
@@ -1,18 +1,36 @@
 'use client';
-// ^ needs to be client side in order to render dates in the user's timezone
 
 import { formatISO9075, formatRelative } from 'date-fns';
+import { Suspense, useEffect, useState } from 'react';
 
 type TimestampProps = {
   /** A unix timestamp (seconds) in the past */
   children: number;
 };
 
+/**
+ * A few tricks to fix local vs. server timestamps from here:
+ * https://francoisbest.com/posts/2023/displaying-local-times-in-nextjs
+ */
 export default function Timestamp({ children }: TimestampProps) {
   const date = new Date(children * 1000);
+  const hydrated = useHydration();
+
   return (
-    <span title={formatISO9075(date)} suppressHydrationWarning>
-      {formatRelative(date, new Date())}
-    </span>
+    <Suspense key={hydrated ? 'local' : 'utc'}>
+      <span title={formatISO9075(date)} suppressHydrationWarning>
+        {formatRelative(date, new Date())}
+      </span>
+    </Suspense>
   );
+}
+
+function useHydration() {
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  return hydrated;
 }
